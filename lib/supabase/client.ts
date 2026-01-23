b4399a8b0f69253194142103e5206436a6fbf013
lhernandez <lhernandez@swisherdominicana.com>
2026-01-23 11:18:36 -0400

    test 2

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -5,18 +5,21 @@ import { createBrowserClient } from "@supabase/ssr"
 let client: ReturnType<typeof createBrowserClient> | null = null
 
 export function createClient() {
-  if (client) return client
 
-  client = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
   if (
   !process.env.NEXT_PUBLIC_SUPABASE_URL ||
   !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 ) {
   throw new Error("Supabase env vars are missing")
 }
+
+  if (client) return client
+
+  client = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+
 
   return client
 }
