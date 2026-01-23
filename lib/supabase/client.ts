bd8275fd5e6a0e50b5af185de5608a3e89496e3d
lhernandez <lhernandez@swisherdominicana.com>
2026-01-23 11:16:42 -0400

    test

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -11,6 +11,12 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
+  if (
+  !process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+) {
+  throw new Error("Supabase env vars are missing")
+}
 
   return client
 }
